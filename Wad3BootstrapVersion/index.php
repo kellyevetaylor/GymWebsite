2ba536540786d5863ba2c6b9070575fa50af2eb4
pavindersingh <dkb15175@uni.strath.ac.uk>
2017-11-14 20:59:49 +0000

    updated the about section on the index page and also
    Signed-off-by: pavindersingh <dkb15175@uni.strath.ac.uk>

--- Wad3BootstrapVersion/index.php
+++ Wad3BootstrapVersion/index.php
@@ -12,8 +12,8 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="the index page, information regarding the WAD Gym, login and re-direct for signing up" content="">
+    <meta name="cs312" content="">
 
     <title>WAD 3 - Gym</title>
 
@@ -27,6 +27,24 @@
 
     <!-- Custom styles for this template -->
     <link href="css/grayscale.min.css" rel="stylesheet">
+
+    <script>
+        function validateInputForm() {
+            var inUsername = document.forms["loginForm"]["Username"].value;
+            var inPassword = document.forms["loginForm"]["Password"].value;
+            var message = "Please enter your :\n";
+            if(inUsername == ""){
+                message = message + "Username\n";
+            }
+            if(inPassword == ""){
+                message = message + "Password\n";
+            }
+            if (inUsername == "" || inPassword == "") {
+                alert(message);
+                return false;
+            }
+        }
+    </script>
 
 </head>
 
@@ -83,14 +101,16 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8 mx-auto">
-                <h2>About Grayscale</h2>
-                <p>Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
-                    <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
-                <p>This theme features stock photos by
-                    <a href="http://gratisography.com/">Gratisography</a>
-                    along with a custom Google Maps skin courtesy of
-                    <a href="http://snazzymaps.com/">Snazzy Maps</a>.</p>
-                <p>Grayscale includes full HTML, CSS, and custom JavaScript files along with SASS and LESS files for easy customization!</p>
+                <h2>About WAD GYM</h2>
+                <p>
+                    We have over 500 gyms across UK, each gym contains over 30 personal trainers
+                    <br>
+                    who are kept up to date with the latest equipment and health knowledge
+                    <br>
+                    our gym is also kept up to date and have regular inspections
+                    <br>
+                    to make sure each gym meets our high standard
+                <p>
             </div>
         </div>
     </div>
@@ -98,31 +118,32 @@
 
 <!-- Download Section -->
 <section id="signInUp" class="download-section content-section text-center">
-    <div class="container">
-        <div class="col-lg-8 mx-auto">
-            <p>
-            <h2>Account</h2>
-            <ul class="list-inline banner-social-buttons">
-                <li class="list-inline-item">
-                    <input type="text" name="Username" placeholder="Username"/>
-                </li>
-            </ul>
-            <ul class="list-inline banner-social-buttons">
-                <li class="list-inline-item">
-                    <input type="password" name="Password" placeholder="Password"/>
-                </li>
-            </ul>
-            <ul class="list-inline banner-social-buttons">
-                <li class="list-inline-item">
+        <div class="container">
+            <div class="col-lg-8 mx-auto">
+                <p>
+                <h2>Account</h2>
+                <form name="loginForm" action="Dashboard.php" method="post" onsubmit="return validateInputForm()">
+                    <ul class="list-inline banner-social-buttons">
+                        <li class="list-inline-item">
+                            <input type="text" name="Username" placeholder="Username"/>
+                        </li>
+                    </ul>
+                    <ul class="list-inline banner-social-buttons">
+                        <li class="list-inline-item">
+                            <input type="password" name="Password" placeholder="Password"/>
+                        </li>
+                    </ul>
                     <input type="submit" name="SignIn" value="Sign In" class="btn btn-default btn-lg"/>
-                </li>
-                <li class="list-inline-item">
-                    <input type="submit" name="SignIn" value="Sign Up" class="btn btn-default btn-lg"/>
-                </li>
-            </ul>
-            </p>
+                </form>
+                <br>
+                <form>
+                        <input type="submit" href="test" name="SignIn" value="Sign Up" class="btn btn-default btn-lg"/>
+                </form>
+                </ul>
+                </p>
+            </div>
         </div>
-    </div>
+
 </section>
 
 <!-- Contact Section -->
@@ -158,7 +179,7 @@
 <!-- Footer -->
 <footer>
     <div class="container text-center">
-        <p>Copyright &copy; Your Website 2017</p>
+        <p>Copyright &copy; WAD Gym 2017</p>
     </div>
 </footer>
 
