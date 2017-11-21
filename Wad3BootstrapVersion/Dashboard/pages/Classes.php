a7ce20c7305633c55e8d806a5cde673a737cfefb
pavindersingh <dkb15175@uni.strath.ac.uk>
2017-11-21 14:44:55 +0000

    added classes file
    Signed-off-by: pavindersingh <dkb15175@uni.strath.ac.uk>

---
+++ Wad3BootstrapVersion/Dashboard/pages/Classes.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: pavindersingh
+ * Date: 21/11/2017
+ * Time: 14:44
+ */ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
